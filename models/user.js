1df6dba9f4e7f0fd0a0f13d2a1887b57cc0a7ba7
Mayur Darade <mayurdarade99@gmail.com>
2022-05-13 23:58:13 +0530

    few changes

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const serSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   secretCode: String,
 });
 
-export default mongoose.model("User", serSchema);
+export default mongoose.model("User", userSchema);
